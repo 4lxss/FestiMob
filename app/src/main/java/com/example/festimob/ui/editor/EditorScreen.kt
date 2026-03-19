be24366deff7129478bcf30c36208854d65979d8
Caroline Floquet <caroline.floquet@etu.umontpellier.fr>
2026-03-19 18:33:15 +0100

    feat:base of EditorScreen

--- app/src/main/java/com/example/festimob/ui/editor/EditorScreen.kt
+++ app/src/main/java/com/example/festimob/ui/editor/EditorScreen.kt
@@ -1,22 +1,40 @@
 package com.example.festimob.ui.editor
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.festimob.data.models.Editor
 
 @Composable
-fun EditorScreen() {
-    Box(
-        modifier = Modifier.padding(innerPadding)
-    ) {
-        Text("Editors")
+fun EditorScreen(
+    // Inject the ViewModel using the Factory we defined
+    viewModel: EditorViewModel = viewModel(factory = EditorViewModel.Factory)
+) {
+    // Collect the UI state safely
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
-    }
+    EditorList(editors = uiState.editorsList)
 }
 
 @Composable
-fun EditorList() {
-
+fun EditorList(
+    editors: List<Editor>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = modifier) {
+        items(items = editors, key = { it.id }) { editor ->
+            // This is where you design your individual row
+            Text(
+                text = editor.name,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
 }
