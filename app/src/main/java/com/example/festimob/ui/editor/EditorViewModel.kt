d3b3a40c50c3c3013a1a096e3fe425bba868caec
Caroline Floquet <caroline.floquet@etu.umontpellier.fr>
2026-03-19 18:19:15 +0100

    feat:base viewmodel

--- app/src/main/java/com/example/festimob/ui/editor/EditorViewModel.kt
+++ app/src/main/java/com/example/festimob/ui/editor/EditorViewModel.kt
@@ -1,44 +1,22 @@
 package com.example.festimob.ui.editor
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.lifecycle.viewModelScope
+import com.example.festimob.data.models.Editor
 import com.example.festimob.data.repositories.EditorsRepository
-import com.example.festimob.data.repositories.OfflineEditorsRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+data class EditorUiState(val editorsList: List<Editor> = emptyList())
 
-class EditorViewModel(
-    private val editorsRepository : EditorsRepository
-) : ViewModel() {
-    /*
-    // UI states access for various [DessertReleaseUiState]
-    val uiState: StateFlow<EditorsUiState> = userPreferencesRepository.isLinearLayout.map { isLinearLayout ->
-        DessertReleaseUiState(isLinearLayout)
-    }
+class EditorViewModel(private val editorsRepository: EditorsRepository) : ViewModel() {
+
+    val uiState: StateFlow<EditorUiState> = editorsRepository.getEditorsStream()
+        .map { EditorUiState(it) }
         .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = DessertReleaseUiState()
+            initialValue = EditorUiState()
         )
-    */
-    /*
-     * [selectLayout] change the layout and icons accordingly and
-     * save the selection in DataStore through [userPreferencesRepository]
-     */
-    /*
-    fun selectLayout(isLinearLayout: Boolean) {
-        viewModelScope.launch {
-            userPreferencesRepository.saveLayoutPreference(isLinearLayout)
-        }
-    }
-    */
-    companion object {
-        val Factory: ViewModelProvider.Factory = viewModelFactory {
-            initializer {
-                val application = (this[APPLICATION_KEY] as FestiMobApplication)
-                EditorViewModel(application.editorsRepository)
-            }
-        }
-    }
 }
