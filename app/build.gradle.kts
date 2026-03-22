50674438aa56c6aebd0c8eea1da2387317999f1c
Caroline Floquet <caroline.floquet@etu.umontpellier.fr>
2026-03-22 20:47:08 +0100

    fix:navigation 3 ui import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,6 @@ dependencies {
     implementation(libs.androidx.datastore.preferences)
 
     implementation(libs.androidx.compose.material.icons.extended)
+
+    implementation(libs.androidx.navigation3.ui)
 }
