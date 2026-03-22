ac0e24f2c168385e099b5630ef3d3115bea5226a
Caroline Floquet <caroline.floquet@etu.umontpellier.fr>
2026-03-22 20:22:48 +0100

    feat:add EditorListGridLayout

--- app/src/main/java/com/example/festimob/ui/editor/EditorScreen.kt
+++ app/src/main/java/com/example/festimob/ui/editor/EditorScreen.kt
@@ -1,10 +1,19 @@
 package com.example.festimob.ui.editor
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,14 +24,18 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.festimob.R
+import com.example.festimob.data.local.LocalDataPlaceholder
 import com.example.festimob.data.models.Editor
 /*
 @Composable
@@ -113,8 +126,42 @@ fun EditorScreen(
 }
 
 @Composable
-fun EditorListGridLayout(modifier: Modifier, contentPadding: PaddingValues) {
-    TODO("Not yet implemented")
+fun EditorListGridLayout(
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Fixed(3),
+        contentPadding = contentPadding,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
+    ) {
+        items(
+            items = LocalDataPlaceholder.editorsPlaceholderData,
+            key = { editor -> editor.id }
+        ) { editor ->
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                modifier = Modifier.height(110.dp),
+                shape = MaterialTheme.shapes.medium
+            ) {
+                Text(
+                    text = editor.name,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .wrapContentHeight(Alignment.CenterVertically)
+                        .padding(dimensionResource(R.dimen.padding_small))
+                        .align(Alignment.CenterHorizontally),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
 }
 
 @Composable
