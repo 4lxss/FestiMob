a989b212cac1abed93063c8cc499522bc6c3a81b
Alessandra <alessandra.van-rossen-martinez@etu.umontpellier.fr>
2026-03-23 08:44:55 +0100

    feat: cleaned the festival screen file

--- app/src/main/java/com/example/festimob/ui/screens/FestivalListScreen.kt
+++ app/src/main/java/com/example/festimob/ui/screens/FestivalListScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.festimob.ui.viewmodels.FestivalListViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
