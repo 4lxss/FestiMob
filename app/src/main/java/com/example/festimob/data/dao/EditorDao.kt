c7b18361aa69a135c2d477a1da0c45299816e6a4
Caroline Floquet <caroline.floquet@etu.umontpellier.fr>
2026-03-31 11:53:51 +0200

    feat:insert function in dao

--- app/src/main/java/com/example/festimob/data/dao/EditorDao.kt
+++ app/src/main/java/com/example/festimob/data/dao/EditorDao.kt
@@ -1,6 +1,8 @@
 package com.example.festimob.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.festimob.data.dto.EditorDto
 import kotlinx.coroutines.flow.Flow
@@ -11,4 +13,7 @@ interface EditorDao {
     // Using Flow ensures the UI stays in sync with the DB automatically
     @Query("SELECT * FROM editors ORDER BY name ASC")
     fun getAllEditors(): Flow<List<EditorDto>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertEditor(editor: EditorDto)
 }
