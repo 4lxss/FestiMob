80a55a8d922d158c5a78ec9bf5347ac8543f7856
Caroline Floquet <caroline.floquet@etu.umontpellier.fr>
2026-04-04 17:02:19 +0200

    feat:added editor details base and viewmodel

--- app/src/main/java/com/example/festimob/ui/editor/EditorDetailsScreen.kt
+++ app/src/main/java/com/example/festimob/ui/editor/EditorDetailsScreen.kt
@@ -1,2 +1,16 @@
 package com.example.festimob.ui.editor
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun EditorDetailsScreen(
+    editorId: Int
+) {
+    Spacer(Modifier.padding(4.dp))
+    Text("Congrats on reaching Editor $editorId Details screen")
+}
--- app/src/main/java/com/example/festimob/ui/editor/EditorDetailsViewModel.kt
+++ app/src/main/java/com/example/festimob/ui/editor/EditorDetailsViewModel.kt
@@ -1,2 +1,37 @@
 package com.example.festimob.ui.editor
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.festimob.FestiMobApplication
+import com.example.festimob.data.models.Editor
+import com.example.festimob.data.repositories.EditorsRepository
+
+data class EditorDetailsUiState(
+    val editor: Editor?
+)
+
+class EditorDetailsViewModel(
+    private val editorsRepository: EditorsRepository
+) : ViewModel() {
+    var uiState by mutableStateOf(EditorDetailsUiState(editor = null))
+
+    fun setUiState(editor: Editor) {
+        uiState = EditorDetailsUiState(editor)
+    }
+
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as FestiMobApplication)
+                EditorDetailsViewModel(application.editorsRepository)
+            }
+        }
+    }
+}
