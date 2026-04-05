c59eda0a1aebb81a0131c978bd18b08b26be2ea4
Caroline Floquet <caroline.floquet@etu.umontpellier.fr>
2026-04-05 23:49:46 +0200

    feat:track active tab

--- app/src/main/java/com/example/festimob/ui/editor/EditorDetailsScreen.kt
+++ app/src/main/java/com/example/festimob/ui/editor/EditorDetailsScreen.kt
@@ -4,6 +4,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -13,6 +17,9 @@ fun EditorDetailsScreen(
     viewModel: EditorDetailsViewModel
 ) {
     viewModel.setUiState(editorId)
+    var selectedTabIndex by rememberSaveable { mutableIntStateOf(0) }
+
+
     Spacer(Modifier.padding(4.dp))
     Text("Congrats on reaching Editor ${viewModel.uiState.editor?.id} Details screen")
 }
