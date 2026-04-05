8e11fece2ad722dbcc30e363531ca4f2cd071a82
Caroline Floquet <caroline.floquet@etu.umontpellier.fr>
2026-04-06 00:26:38 +0200

    feat:added internal top bar

--- app/src/main/java/com/example/festimob/ui/editor/EditorDetailsScreen.kt
+++ app/src/main/java/com/example/festimob/ui/editor/EditorDetailsScreen.kt
@@ -1,16 +1,33 @@
 package com.example.festimob.ui.editor
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.PrimaryTabRow
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.room.util.TableInfo
+import com.example.festimob.R
+import com.example.festimob.ui.theme.AccentTurquoise
+import com.example.festimob.ui.theme.DarkTealBackground
+import com.example.festimob.ui.theme.TextWhite
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditorDetailsScreen(
     editorId : Int,
@@ -19,9 +36,50 @@ fun EditorDetailsScreen(
     viewModel.setUiState(editorId)
     var selectedTabIndex by rememberSaveable { mutableIntStateOf(0) }
 
+    val editor = viewModel.uiState.editor
 
-    Spacer(Modifier.padding(4.dp))
-    Text("Congrats on reaching Editor ${viewModel.uiState.editor?.id} Details screen")
+    val title = editor?.name ?: "Unknown editor"
+
+    Scaffold(
+        containerColor = DarkTealBackground,
+        topBar = {
+            TopAppBar(
+                title = { Text(title,color = TextWhite) },
+                actions = {
+                    /*
+                    IconButton(
+                        onClick = {
+                            viewModel.selectLayout(!isLinearLayout)
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(uiState.toggleIcon),
+                            contentDescription = stringResource(uiState.toggleContentDescription),
+                            tint = AccentTurquoise
+                        )
+                    }
+                    */
+                },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = DarkTealBackground
+                )
+            )
+        },
+    ) { innerPadding ->
+        val hereModifier = Modifier
+            .padding(
+                top = dimensionResource(R.dimen.padding_medium),
+                start = dimensionResource(R.dimen.padding_medium),
+                end = dimensionResource(R.dimen.padding_medium),
+            )
+        Column(
+            modifier = hereModifier
+        ) {
+            Spacer(hereModifier.padding(innerPadding))
+            Text("Congrats on reaching Editor ${viewModel.uiState.editor?.id} Details screen", color = TextWhite)
+        }
+    }
+
 }
 
 enum class EditorTab(val title: String) {
