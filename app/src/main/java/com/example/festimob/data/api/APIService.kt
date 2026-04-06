84433d01418a01e376030901160a99bf539a9bc2
nour <nour@nours-MacBook-Air.local>
2026-04-06 12:18:46 +0200

    Add Retrofit GET methods for jeux and mecanisms lists

--- app/src/main/java/com/example/festimob/data/api/APIService.kt
+++ app/src/main/java/com/example/festimob/data/api/APIService.kt
@@ -5,4 +5,10 @@ import retrofit2.http.GET
 interface APIService {
     @GET("api/festivals/all")
     suspend fun getFestivals() : List<Festival>
+
+    @GET("api/jeux/all")
+    suspend fun getJeux(): List<JeuDto>
+
+    @GET("api/mecanisms/all")
+    suspend fun getMecanisms(): List<MecanismDto>
 }
