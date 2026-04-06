c01f75ce03d215fb49e6bcb46ee2654c0bfb6e99
Nayarr <rayanluffy30@gmail.com>
2026-04-06 19:59:52 +0200

    feat: add data models for editor requests and responses

--- app/src/main/java/com/example/festimob/games/GamesScreen.kt
+++ app/src/main/java/com/example/festimob/games/GamesScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -95,12 +96,6 @@ fun GamesScreen(
                     .fillMaxWidth()
                     .padding(16.dp)
             ) {
-                Text(
-                    text = "Games",
-                    style = MaterialTheme.typography.headlineSmall,
-                    fontWeight = FontWeight.Bold
-                )
-
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
